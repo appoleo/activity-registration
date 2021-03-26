f68f9fb7b6e873df2dff2364a9ca431b54c4fe98
wangchendong <appoleo@163.com>
2021-03-26 21:12:38 +0800

    去除多余依赖

--- src/main/java/com/me/study/StudyApplication.java
+++ src/main/java/com/me/study/StudyApplication.java
@@ -1,6 +1,5 @@
 package com.me.study;
 
-import com.apple.eawt.Application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
